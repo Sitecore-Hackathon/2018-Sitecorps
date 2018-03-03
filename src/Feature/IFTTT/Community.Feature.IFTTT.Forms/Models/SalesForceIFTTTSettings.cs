22ac10797bb0f06d0c6faa45969b9f55589d4923
mohammed.syam <mohammed.syam@americaneagle.com>
2018-03-03 17:00:56 +0200

    Sitecore IFTTT + sales force intergtaion initial commit,

---
+++ src/Feature/IFTTT/Community.Feature.IFTTT.Forms/Models/SalesForceIFTTTSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Community.Feature.IFTTT.Forms.Models
+{
+    public class SalesForceIFTTTSettings
+    {
+        public string accountid { get; set; }
+        public string eventid { get; set; }
+    }
+}
--- src/Feature/IFTTT/Community.Feature.IFTTT.Forms/Sitecore Forms Custom Actions/SalesForceLeadAction.cs
+++ src/Feature/IFTTT/Community.Feature.IFTTT.Forms/Sitecore Forms Custom Actions/SalesForceLeadAction.cs
@@ -4,6 +4,13 @@ using Sitecore.ExperienceForms.Models;
 using Sitecore.ExperienceForms.Processing;
 using Sitecore.ExperienceForms.Processing.Actions;
 using static System.FormattableString;
+using Community.Feature.IFTTT.Services;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Community.Feature.IFTTT.Forms.Models;
+using Sitecore.Data.Items;
+using Sitecore.Configuration;
 
 namespace Community.Feature.IFTTT.Forms.Sitecore_Forms_Custom_Actions
 {
@@ -19,6 +26,9 @@ namespace Community.Feature.IFTTT.Forms.Sitecore_Forms_Custom_Actions
 
         }
 
+        internal Item AccountItem { get; set; }
+        internal Item EventItem { get; set; }
+
         /// <summary>
         /// Tries to convert the specified <paramref name="value" /> to an instance of the specified target type.
         /// </summary>
@@ -33,6 +43,25 @@ namespace Community.Feature.IFTTT.Forms.Sitecore_Forms_Custom_Actions
             return true;
         }
 
+        public override void ExecuteAction(FormSubmitContext formSubmitContext, string parameters)
+        {
+            SalesForceIFTTTSettings _salesForceIFTTTSettings =
+                JsonConvert.DeserializeObject<SalesForceIFTTTSettings>(parameters);
+
+            if (_salesForceIFTTTSettings != null)
+            {
+                AccountItem =
+                    Factory.GetDatabase(Constants.DBNames.masterDB).GetItem(_salesForceIFTTTSettings.accountid);
+
+                EventItem =
+                    Factory.GetDatabase(Constants.DBNames.masterDB).GetItem(_salesForceIFTTTSettings.eventid);
+
+            }
+
+            base.ExecuteAction(formSubmitContext, parameters);
+        }
+
+
         /// <summary>
         /// Executes the action with the specified <paramref name="data" />.
         /// </summary>
@@ -43,16 +72,49 @@ namespace Community.Feature.IFTTT.Forms.Sitecore_Forms_Custom_Actions
         /// </returns>
         protected override bool Execute(string data, FormSubmitContext formSubmitContext)
         {
-            Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
-            if (!formSubmitContext.HasErrors)
+            try
             {
-                Logger.Info(Invariant($"Form {formSubmitContext.FormId} submitted successfully."), this);
+                Assert.ArgumentNotNull(formSubmitContext, nameof(formSubmitContext));
+                Assert.ArgumentNotNull(AccountItem, nameof(AccountItem));
+                Assert.ArgumentNotNull(EventItem, nameof(EventItem));
+
+                if (AccountItem == null || EventItem == null)
+                    throw new Exception("Account Item or/and Event Item are not exist..");
+                
+                var makerKey = string.Empty;
+                var eventName = string.Empty;
+
+                makerKey = AccountItem.Fields[Templates.Templates.Account.Fields.MakerKey].Value;
+                eventName = EventItem.Fields[Templates.Templates.Event.Fields.EventName].Value;
+
+                if (string.IsNullOrWhiteSpace(makerKey) || string.IsNullOrWhiteSpace(eventName))
+                    throw new Exception("Account value or/and Event value are empty..");
+
+                var value1 = ((Sitecore.ExperienceForms.Mvc.Models.Fields.InputViewModel<string>)(formSubmitContext.Fields[0])).Value;
+                var value2 = ((Sitecore.ExperienceForms.Mvc.Models.Fields.InputViewModel<string>)(formSubmitContext.Fields[1])).Value;
+                var value3 = ((Sitecore.ExperienceForms.Mvc.Models.Fields.InputViewModel<string>)(formSubmitContext.Fields[2])).Value;
+
+                var _values = new string[] { value1, value2, value3 };
+
+                // call IFTTT service
+                var iftttService = Sitecore.DependencyInjection.ServiceLocator.ServiceProvider.GetService<IIftttService>();
+                iftttService.Trigger(makerKey, eventName, _values);
+
+                if (!formSubmitContext.HasErrors)
+                {
+                    Logger.Info(Invariant($"Form {formSubmitContext.FormId} submitted successfully."), this);
+                }
+                else
+                {
+                    Logger.Warn(Invariant($"Form {formSubmitContext.FormId} submitted with errors: {string.Join(", ", formSubmitContext.Errors.Select(t => t.ErrorMessage))}."), this);
+                }
+                return true;
             }
-            else
+            catch (Exception ex)
             {
-                Logger.Warn(Invariant($"Form {formSubmitContext.FormId} submitted with errors: {string.Join(", ", formSubmitContext.Errors.Select(t => t.ErrorMessage))}."), this);
+                Sitecore.Diagnostics.Log.Error(ex.Message, ex, this);
+                return false;
             }
-            return true;
         }
     }
 }
